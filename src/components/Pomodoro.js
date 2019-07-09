76d09be7a975c5ec823289e934d4196e24191d6c
BulbCat.David <tiida541108@gmail.com>
2019-07-09 23:10:35 +0800

    [ADD] main init
    main init

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import styles from '../scss/Pomodoro.module.scss';
+
 function Pomodoro() {
   return (
-    <div>
-    <h1>Hexschool_TheF2E_2019</h1>
+    <div className={styles.main_01}>
+      <div className={styles.content_left}>
+      <input type="text" className={styles.add_mission} name="addMission" placeholder="add a new mission…"/>
+      </div>
+      <div className={styles.content_center}>
+        <div className={styles.content_center_player}>
+          <svg className={styles.secondCircle} >
+            <circle id="secondCircle" cx="50%" cy="50%" r="260px" stroke-width="26px"></circle>
+          </svg>
+          {/* <span id="secondText" class="time">00</span><span class="time-info">s</span> */}
+        </div>
+      </div>
+
+      <div className={styles.content_right}>
+        <div className={styles.content_right_list}>
+          <i className="material-icons">list</i>
+          <i className="material-icons">insert_chart</i>
+          <i className="material-icons">library_music</i>
+          <span>POMODORO</span>
+        </div>
+      </div>
+
     </div>
   );
 }
+
 
 export default Pomodoro;
